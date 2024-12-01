38b6af7c409c2f2db9b9dc774cf0a40dcd081564
Andrey Chirkin <chirkin.andrey377@gmail.com>
2024-12-01 22:21:36 +0300

    Modified files

--- app/handlers/quiz/start_quiz.py
+++ app/handlers/quiz/start_quiz.py
@@ -16,7 +16,7 @@ async def command_start_handler(message: Message, state: FSMContext) -> None:
     await state.set_state(QuizStates.start_quiz)
     await bot.send_photo(
         chat_id=message.chat.id,
-        photo=FSInputFile('Data/images/animals/panda_katyusha.jpeg'),
+        photo=FSInputFile('data/images/animals/panda_katyusha.jpeg'),
         caption=f"Привет, <b>{html.bold(message.from_user.full_name)}!</b>\n"
                 f"Я Катюша, символ и гордость Московского зоопарка. \nЯ очень рада тебя видеть!\n"
                 f"Твой внутренний зверь так и рвется на волю!\n"
@@ -26,7 +26,7 @@ async def command_start_handler(message: Message, state: FSMContext) -> None:
 
 @start_quiz_router.message(QuizStates.start_quiz, F.text == 'Нет, но я бы очень хотел это узнать!')
 async def start_quiz_handler(message: Message, state: FSMContext):
-    photo = FSInputFile('Data/images/logo/MZoo-logo-сircle-universal-preview.jpg')
+    photo = FSInputFile('data/images/logo/MZoo-logo-сircle-universal-preview.jpg')
     await state.set_state(QuizStates.quiz)
     await bot.send_photo(
         chat_id=message.chat.id,
